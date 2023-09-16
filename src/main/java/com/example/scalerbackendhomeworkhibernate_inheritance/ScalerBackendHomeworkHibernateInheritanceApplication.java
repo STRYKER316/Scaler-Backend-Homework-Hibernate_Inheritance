204b6d4883c01924d574f3b09ed8642b769aff66
Stryker316 <ansil.mishra316@gmail.com>
2023-09-16 19:10:20 +0530

    Initial Setup

---
+++ src/main/java/com/example/scalerbackendhomeworkhibernate_inheritance/ScalerBackendHomeworkHibernateInheritanceApplication.java
@@ -0,0 +1,13 @@
+package com.example.scalerbackendhomeworkhibernate_inheritance;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ScalerBackendHomeworkHibernateInheritanceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ScalerBackendHomeworkHibernateInheritanceApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/scalerbackendhomeworkhibernate_inheritance/ScalerBackendHomeworkHibernateInheritanceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.scalerbackendhomeworkhibernate_inheritance;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ScalerBackendHomeworkHibernateInheritanceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
